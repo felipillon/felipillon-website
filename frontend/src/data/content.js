160134a692413f737ba233c7614c015fbb8d6d2a
Dereck Boateng <cobbyboateng16@gmail.com>
2026-08-31 11:52:08 +0200

    changes

--- frontend/src/data/content.js
+++ frontend/src/data/content.js
@@ -89,12 +89,6 @@ export const HERO_VIDEO = {
 
 export const HERO_VIDEOS = [
   {
-    label: "Felipillon",
-    src: HERO_VIDEO.src,
-    poster: HERO_VIDEO.poster,
-    maxDuration: 7, // stop before logo appears at bottom
-  },
-  {
     label: "Construction",
     src: "https://videos.pexels.com/video-files/1197802/1197802-hd_1920_1080_25fps.mp4",
     poster: "https://images.pexels.com/videos/1197802/free-video-1197802.jpg?auto=compress&cs=tinysrgb&w=1920",
